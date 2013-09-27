0ab5b1672d1700571830c3577e7cb49d147844d8
kanishka <nish2575@gmail.com>
2013-09-27 11:56:05 -0400

    finish default instances

--- Types.hs
+++ Types.hs
@@ -124,26 +124,69 @@ seniorityFieldDetail = PatronageFieldDetail "seniority"
 qualityFieldDetail = PatronageFieldDetail "quality" 
 revenueGeneratedFieldDetail = PatronageFieldDetail "revenueGenerated" 
 
+instance Default PeriodType where
+  def = Year
+
+instance Default GregorianMonth where
+  def = GregorianMonth 1970 1
+  
+instance Default GregorianDuration where
+  def = GregorianDuration 0 0
+
+instance Default AllocationMethod where
+  def = ProductiveHours
+
+instance Default FiscalPeriod where
+  def = FiscalPeriod{start=def,periodType=Year}
+
+instance Default Day where
+  def = fromGregorian 1970 1 1
+  
+instance Default EquityActionType where
+  def = AllocatePatronageRebate
+
 instance Default PatronageWeights where 
   def = PatronageWeights{workw=1,skillWeightedWorkw=def,seniorityw=def,qualityw=def,
                          revenueGeneratedw=def}
+
 instance Default WorkPatronage where
-  def = WorkPatronage{work=0,skillWeightedWork=0,
-                               seniority=0,quality=0,revenueGenerated=0,
-                               performedOver=FiscalPeriod (GregorianMonth 1970 1) Year}
+  def = WorkPatronage{work=0,skillWeightedWork=0,seniority=0,quality=0,revenueGenerated=0,
+                      performedOver=def}
+
+instance Default MemberEquityAction where
+  def = MemberEquityAction{actionType=def, amount=def,performedOn=def}
+
+instance Default EquityAccountType where
+  def = RollingPatronageAcct
+
+instance Default MemberEquityAccount where
+  def = MemberEquityAccount{ida=1,accountType=def}
+
+instance Default Member where
+  def = Member{firstName=def,lastName=def,memberId=1,acceptedOn=def}
+  
+instance Default FinancialResults where
+  def = FinancialResults{over=def,surplus=def,allocatedOn=def}
+  
+instance Default FiscalCalendarType where
+  def = FiscalCalendarType{startf=1,periodTypef=def}
+
+instance Default Cooperative where
+  def = Cooperative{cooperativeId=1,name=def,username=def,usageStart=def,usageEnd=def,
+                    fiscalCalendarType=def}
+
 
 -- sample data
 (f1,f2) = 
-  (FiscalPeriod (GregorianMonth 2012 1) Year,
-   FiscalPeriod (GregorianMonth 2011 1) Year)
+  (def{start=(GregorianMonth 2012 1)},
+   def{start=(GregorianMonth 2011 1)})::(FiscalPeriod,FiscalPeriod)
 (m1, m2, m3) = 
    (Member "John" "Smith" 1 (fromGregorian 2010 1 1), 
     Member "Kanishka" "Azimi" 2 (fromGregorian 2011 1 1), 
-    Member "Dave" "Jackson" 3 (fromGregorian 2009 3 30))
-pw1 = PatronageWeights (7%10) (3%10) 0 0 0
-coop1 = (Cooperative 
-           1 "Coop1" "https://www.google.com/profiles/102678013619698873278"
-           (fromGregorian 2010 1 1) Nothing (FiscalCalendarType 1 Year))
+    Member "Dave" "Jackson" 3 (fromGregorian 2009 3 30))::(Member,Member,Member)
+pw1 = def{workw=7%10,skillWeightedWorkw=3%10}::PatronageWeights
+coop1 = def{name="Coop1",username="https://www.google.com/profiles/102678013619698873278",
+             usageStart=fromGregorian 2010 1 1}
 settings1 = (SimpleMix, 
              pw1,
              [(GregorianDuration 1 0, 6%10), (GregorianDuration 1 6, 4%10)])
@@ -153,7 +196,5 @@ memPatronage1 = (M.fromList
                   , (m2, [WorkPatronage 10 20 30 40 50 f2])
                   , (m3, [WorkPatronage 100 200 300 400 500 f2]) ])
 res1 = [FinancialResults 
-          (FiscalPeriod (GregorianMonth 2012 1) Year) 
-          200
-          $ Just (fromGregorian 2011 1 2)] 
-acct1 = MemberEquityAccount 1 RollingPatronageAcct
+          def{start=GregorianMonth 2012 1} 200 $ Just (fromGregorian 2011 1 2)] 
+acct1 = def::MemberEquityAccount
